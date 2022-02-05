0f868ed35b96a17dc87e9d9e988d334715e8d3e3
Michał Szymański <michal.szymanski1908@gmail.com>
2022-02-05 17:18:46 +0100

    Before final task

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.render('index.hbs');
 });
@@ -30,6 +34,19 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res, next) => {
   res.render('history.hbs');
+});
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.render('contact', { isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
 });
 
 app.use((req, res) => {
